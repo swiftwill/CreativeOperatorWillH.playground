b0056b71a2b7bcbeb1f8fb6209c7e366cf0bb45d
swiftwill <prc-lh138-18@PRC-LH138-18s-iMac.local>
2016-07-11 16:31:51 -0500

    CreativeOperator
    CriticalThinkingExercise: Creative use of operators we learned about.

---
+++ Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+//  William Hill
+//  Critical Thinking Exercise
+
+
+import UIKit
+
+// variables for opening message
+var str1 = "Hello, Pokie! It's time to get creative!"
+var str2 = "So open up Google and google away!"
+
+// variable declaration and initialization
+var a = 1
+var b = 2
+var c = 3
+var d = 4
+var e = 5
+var f = 6
+var g = 7
+var h = 8
+var i = 9
+var j = 10
+// creative part
+var figuredout = (a + (b - c * d) % (e / f + g) - h / i * j)
+
+// print results
+print("Here are the variable contents used in this problem: ")
+print("a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7, h = 8, i = 9, j = 10")
+print("Figure out how I used those variables to come up with: ", figuredout, terminator:"")
+
+
+
